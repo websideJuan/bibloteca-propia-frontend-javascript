224dc88f82c120b0ea7c1b25944c58594910b797
juanHerrera <portador1051@gmail.com>
2025-06-20 02:34:32 -0400

    deploy whit gh-pages

---
+++ vite.config.js
@@ -0,0 +1,6 @@
+import { defineConfig } from 'vite'
+
+
+export default defineConfig({
+  base: '/websideJuan/bibloteca-propia-frontend-javascript', // This sets the base path for the application, useful for relative paths
+});
