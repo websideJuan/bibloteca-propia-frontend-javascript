7b1cf36af5f20b671a0ceeca80b75a4f41e6a11c
juanHerrera <portador1051@gmail.com>
2025-06-20 04:01:59 -0400

    deploy whit gh-pages error in routing betalpha 2

--- src/routing/routing.js
+++ src/routing/routing.js
@@ -5,17 +5,17 @@ class Routing {
     this.init();
   }
   addRoute({ path, component, callback, redirectTo }) {
-    if (typeof path !== 'string' || !path.startsWith('/')) {
+    if (typeof path !== "string" || !path.startsWith("/")) {
       throw new Error('Path must be a string starting with "/"');
     }
-    if (component && typeof component !== 'function' ) {
-      throw new Error('Component must be a string');
+    if (component && typeof component !== "function") {
+      throw new Error("Component must be a string");
     }
-    if (callback && typeof callback !== 'function') {
-      throw new Error('Callback must be a function');
+    if (callback && typeof callback !== "function") {
+      throw new Error("Callback must be a function");
     }
-    if (redirectTo && typeof redirectTo !== 'string') {
-      throw new Error('RedirectTo must be a string');
+    if (redirectTo && typeof redirectTo !== "string") {
+      throw new Error("RedirectTo must be a string");
     }
 
     this.routes[path] = { component, callback, redirectTo };
@@ -23,19 +23,19 @@ class Routing {
 
   navigate({ path }) {
     if (!this.routes[path]) {
-      this.renderContent(`<h1>404 Not Found</h1> <a href="#/home">Go to Home</a>`);
+      this.renderContent(
+        `<h1>404 Not Found</h1> <a href="#/home">Go to Home</a>`
+      );
       console.error(`Route not found: ${path}`);
       return;
     }
 
     if (this.routes[path]?.callback) {
       const isAuthenticated = this.routes[path].callback();
-     
+
       if (isAuthenticated === false) {
-        
-          this.navigate({ path: '/login' });
-          
-        
+        this.navigate({ path: "/login" });
+
         return;
       }
     }
@@ -46,10 +46,12 @@ class Routing {
     }
 
     if (this.routes[path]) {
-      const content = this.routes[path]?.component ? this.routes[path].component() : '' 
+      const content = this.routes[path]?.component
+        ? this.routes[path].component()
+        : "";
       this.renderContent(content);
-      
-      if (typeof this.routes[path].component.init === 'function') {
+
+      if (typeof this.routes[path].component.init === "function") {
         this.routes[path].component.init();
       }
     }
@@ -58,35 +60,35 @@ class Routing {
     return this.currentRoute;
   }
   init() {
-    window.addEventListener('hashchange', () => this.handleHashChange());
-    window.addEventListener('load', () => this.handleHashChange());
+    window.addEventListener("hashchange", () => this.handleHashChange());
+    window.addEventListener("load", () => this.handleHashChange());
   }
 
   handleHashChange() {
-    const path = window.location.hash.slice(1) || '/';
+    const path = location.hash.slice(1) || "/";
     console.log(path);
-    
+
     this.navigate({ path });
   }
 
   renderContent(content) {
-    const app = document.getElementById('app');
+    const app = document.getElementById("app");
     if (!app) {
-      throw new Error('App element not found');
+      throw new Error("App element not found");
     }
 
-    app.innerHTML = content; 
+    app.innerHTML = content;
 
     this.attachiLinkHandlers();
   }
 
   attachiLinkHandlers() {
-    document.querySelectorAll('a[href^="#/"]').forEach(link => {
-      link.addEventListener('click', (event) => {
+    document.querySelectorAll('a[href^="#/"]').forEach((link) => {
+      link.addEventListener("click", (event) => {
         event.preventDefault();
-        const path = link.getAttribute('href').slice(1); // Remove the leading '#/'
-        
-        window.location.hash = path; // Update the hash in the URL
+        const path = link.getAttribute("href").slice(1); // Remove the leading '#/'
+
+        location.hash = path; // Update the hash in the URL
       });
     });
   }
