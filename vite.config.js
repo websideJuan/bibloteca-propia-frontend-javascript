6d9d60e33a7c3d10c76b23a82ab3865918633986
juanHerrera <portador1051@gmail.com>
2025-06-21 16:26:49 -0400

    chager target es2022

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,5 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
   base: '/bibloteca-propia-frontend-javascript/', // This sets the base path for the application, useful for relative paths
+  target: 'es2022', // This sets the target JavaScript version for the build
 });
